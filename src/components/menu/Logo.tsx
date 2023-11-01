910ea7cba90a3f459857d67b8a98f73979832f6e
MarvelousMarcin <01159356@pw.edu.pl>
2023-11-01 16:07:19 +0100

    fix logo

--- src/components/menu/Logo.tsx
+++ src/components/menu/Logo.tsx
@@ -2,10 +2,8 @@
 
 const Logo = ({}) => {
   return (
-    <div className="flex gap-1 w-full">
-      <div>
-        To<span className="text-rose-500 font-bold">Do</span>
-      </div>
+    <div>
+      To<span className="text-rose-500 font-bold">Do</span>
     </div>
   );
 };
